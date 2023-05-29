7e7b59163d91e9050b8f98efa061f69b8a11f73c
RileyBax <134656864+RileyBax@users.noreply.github.com>
2023-05-29 20:28:37 +1200

    Added Alpha Vantage Scraper

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,4 +1,11 @@
+import java.util.List;
+
 import javax.swing.JFrame;
+
+import io.github.mainstringargs.alphavantagescraper.AlphaVantageConnector;
+import io.github.mainstringargs.alphavantagescraper.TimeSeries;
+import io.github.mainstringargs.alphavantagescraper.input.timeseries.Interval;
+import io.github.mainstringargs.alphavantagescraper.output.timeseries.data.StockData;
 
 public class App extends JFrame{
 
@@ -15,7 +22,17 @@ public class App extends JFrame{
 	public static void main(String[] args) {
 		
 		App frame = new App();
-
+		
+		String apiKey = "ATD3OP6WNYPCRAEY";
+		
+		TimeSeries client = new TimeSeries(new AlphaVantageConnector(apiKey, 3000));
+		
+		List<StockData> data =  client.intraDay("AAPL", Interval.ONE_MIN).getStockData();
+		
+		for(StockData s : data) {
+			System.out.println(s.getDateTime());
+		}
+		
 	}
 
 }
